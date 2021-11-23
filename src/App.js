8e6a856cd0a1afba5c01ce85f1b1b0e23f8e9ffc
Harsh Rajgor <harshrajgor996@gmail.com>
2021-11-23 14:35:17 -0600

    navigation fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Login from './Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navigate } from 'react-router';
 
 function App() {
   return (
     <div className="App" fluid>
-      <Login/>
+      <Navigate to="/login" />
     </div>
   );
 }
