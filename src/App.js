767a43f5a62c02ca4088f708e3cf92ce336318ce
abijithvelaga <abijithvelaga@gmail.com>
2021-11-23 18:15:40 -0600

    Integration of header and create group

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import Header from "./components/Header";
-import CreateGroup from "./components/CreateGroup";
 
 function App() {
   return (
     <>
-      <CreateGroup />
+      <Header />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Navbar,
   Container,
@@ -6,15 +6,69 @@ import {
   Form,
   FormControl,
   Button,
+  InputGroup,
+  Modal,
+  Dropdown,
 } from "react-bootstrap";
 import "./styles/Header.css";
 import { AiFillHome } from "react-icons/ai";
 import { BsPlusCircleFill } from "react-icons/bs";
 import { FiActivity } from "react-icons/fi";
+import { FaSearch } from "react-icons/fa";
 
 const Header = (props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
+      <>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create Group</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <InputGroup className="my-2 ms-2">
+              <InputGroup.Text>Group Name</InputGroup.Text>
+              <FormControl
+                placeholder="Group Name"
+                aria-label="Group Name"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+            <div className="dropdown">
+              <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic-button">
+                  Search Members
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu style={{ backgroundColor: "#73a47" }}>
+                  <InputGroup className="mb-1">
+                    <FormControl
+                      placeholder="Recipient's username"
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Text id="basic-addon2" onClick={searchMembers}>
+                      <FaSearch />
+                    </InputGroup.Text>
+                  </InputGroup>
+                  <Dropdown.Item href="#">Arabic</Dropdown.Item>
+                  <Dropdown.Item href="#">English</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
       <Navbar bg="dark" expand="lg">
         <Container fluid>
           <Navbar.Brand href="#" id="navBrand">
@@ -27,15 +81,15 @@ const Header = (props) => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="#action1" id="navItem1">
+              <Nav.Link id="navItem1">
                 <AiFillHome style={{ marginBottom: "4px" }} />
                 Home
               </Nav.Link>
-              <Nav.Link href="#action2" id="navItem1">
+              <Nav.Link id="navItem1" onClick={handleShow}>
                 <BsPlusCircleFill style={{ marginBottom: "3px" }} /> Create
                 Group
               </Nav.Link>
-              <Nav.Link href="#" id="navItem1">
+              <Nav.Link id="navItem1">
                 <FiActivity style={{ marginBottom: "3px" }} /> Activity
               </Nav.Link>
             </Nav>
@@ -53,6 +107,10 @@ const Header = (props) => {
       </Navbar>
     </>
   );
+};
+
+const searchMembers = (data) => {
+  alert("It is working" + data);
 };
 
 export default Header;
